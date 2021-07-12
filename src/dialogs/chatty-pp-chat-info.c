bb07f05318e8f8cb7dc19935e56aff855ee2b87a
Mohammed Sadiq <sadiq@sadiqpk.org>
2021-07-12 16:59:13 +0530

    pp-chat-info: Don't allow changing SMS avatars
    This should now be done in GNOME contacts, we now always
    load avatar from evolution data server

--- src/dialogs/chatty-pp-chat-info.c
+++ src/dialogs/chatty-pp-chat-info.c
@@ -246,6 +246,7 @@ chatty_pp_chat_info_update (ChattyPpChatInfo *self)
   pp_chat_info_encrypt_changed_cb (self);
 
   if (protocol == CHATTY_PROTOCOL_SMS) {
+    gtk_widget_set_sensitive (self->avatar_button, FALSE);
     gtk_label_set_text (GTK_LABEL (self->user_id_title), _("Phone Number:"));
   } else if (protocol == CHATTY_PROTOCOL_XMPP) {
     gtk_label_set_text (GTK_LABEL (self->user_id_title), _("XMPP ID:"));
