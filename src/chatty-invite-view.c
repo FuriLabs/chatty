8b888af985343bf3b21bcd7f2ee5d598dc62f95f
Mohammed Sadiq <sadiq@sadiqpk.org>
2022-10-10 18:48:56 +0530

    invite-view: Add a translator comment
    Fixes #745

--- src/chatty-invite-view.c
+++ src/chatty-invite-view.c
@@ -69,6 +69,7 @@ chat_invite_name_changed_cb (ChattyInviteView *self)
     return;
 
   name = chatty_item_get_name (CHATTY_ITEM (self->chat));
+  /* TRANSLATORS: %s is the chat room name */
   title = g_strdup_printf (_("Do you want to join “%s”"), name);
   gtk_label_set_label (GTK_LABEL (self->invite_title), title);
 }
