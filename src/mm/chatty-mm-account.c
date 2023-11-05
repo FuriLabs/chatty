99b3c75250241f99cf95aa49d6c186a425a75aaa
Anson Anstee <129466-AnsTee@users.noreply.gitlab.gnome.org>
2023-11-05 18:11:47 +0000

    mm-account: Change SMS validity from 168 minutes to max. allowed value (635040)
    Right now the SMS is only valid for 168 minutes (slightly less than 3 hours).
    This is not optimal, as the other party may have their phone off and would
    never get the SMS. The default is 24 hours, which is slightly better, but
    still not optimal. We are changing it to the max value, 635,040 minutes,
    or around 63 weeks.

--- src/mm/chatty-mm-account.c
+++ src/mm/chatty-mm-account.c
@@ -1472,7 +1472,11 @@ chatty_mm_account_send_message_async (ChattyMmAccount     *self,
   mm_sms_properties_set_text (sms_properties, chatty_message_get_text (message));
   mm_sms_properties_set_number (sms_properties, phone);
   mm_sms_properties_set_delivery_report_request (sms_properties, request_report);
-  mm_sms_properties_set_validity_relative (sms_properties, 168);
+  /*
+   * https://gitlab.freedesktop.org/mobile-broadband/ModemManager/-/blob/201c8533e0e51c2f3ec6c64240d8a5705c26c8d3/src/mm-sms-part-3gpp.c#L307
+   * Max Value for validity relative is 635040, value is in minutes
+   */
+  mm_sms_properties_set_validity_relative (sms_properties, 635040);
 
   if (chatty_utils_get_item_position (G_LIST_MODEL (self->chat_list), chat, &position))
     g_list_model_items_changed (G_LIST_MODEL (self->chat_list), position, 1, 1);
