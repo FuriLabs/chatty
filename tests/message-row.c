5193159e9f0953b76b6d988ecfa28899a7e3016f
Chris Talbot <chris@talbothome.com>
2023-12-22 08:37:30 -0700

    test/message-row: make sure that message-row doesn't render malicious links
    Part-of: <https://gitlab.gnome.org/World/Chatty/-/merge_requests/1287>

--- tests/message-row.c
+++ tests/message-row.c
@@ -24,6 +24,8 @@ test_message_text_markup (void)
     char *text;
     char *markup;
   } data;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wbidi-chars"
   data array[] = {
     {"",""},
     {"abc","abc"},
@@ -67,8 +69,11 @@ test_message_text_markup (void)
     {
      "http://www.example.com/user's-image.png?blah",
      "<a href=\"http://www.example.com/user&apos;s-image.png?blah\">http://www.example.com/user&apos;s-image.png?blah</a>"},
+    {//https://00xbyte.github.io/posts/Don%27t-Believe-Your-Eyes-A-WhatsApp-Clickjacking-Vulnerability/ Make sure that Chatty isn't vulnerable to this
+     "‮https://moc.margatsni.nl//:sptth",
+     "‮https://moc.margatsni.nl//:sptth"},
   };
-
+#pragma GCC diagnostic pop
   for (guint i = 0; i < G_N_ELEMENTS (array); i++) {
     g_autofree char *content = NULL;
     GtkLabel *label;
