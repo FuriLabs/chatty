54c50c93eb9ac297c1fc01c51cba1d778fec5f52
Chris Talbot <chris@talbothome.com>
2024-02-12 20:21:26 -0700

    utils: Move strip_utm and find_url to utils, add strip_utm_from_message
    Part-of: <https://gitlab.gnome.org/World/Chatty/-/merge_requests/1313>

--- src/chatty-utils.c
+++ src/chatty-utils.c
@@ -20,6 +20,7 @@
 #include "chatty-phone-utils.h"
 #include "chatty-utils.h"
 #include <libebook-contacts/libebook-contacts.h>
+#include <ctype.h>
 #define GNOME_DESKTOP_USE_UNSTABLE_API
 #include <libgnome-desktop/gnome-desktop-thumbnail.h>
 
@@ -28,6 +29,222 @@
 #define DIGITS      "0123456789"
 #define ASCII_CAPS  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #define ASCII_SMALL "abcdefghijklmnopqrstuvwxyz"
+
+#define URL_WWW "www"
+#define URL_HTTP "http"
+#define URL_HTTPS "https"
+#define URL_FILE "file"
+
+#define get_url_start(_start, _match, _type, _suffix, _out)             \
+  do {                                                                  \
+    char *_uri = _match - strlen (_type);                               \
+    size_t len = strlen (_type _suffix);                                \
+                                                                        \
+    if (*_out)                                                          \
+      break;                                                            \
+                                                                        \
+    if (_match - _start >= strlen (_type) &&                            \
+        g_ascii_strncasecmp (_uri, _type _suffix, len) == 0 &&          \
+        (isalnum (_uri[len]) || _uri[len] == '/'))                      \
+      *_out = match - strlen (_type);                                   \
+  } while (0)
+
+char *
+chatty_utils_find_url (const char  *buffer,
+                       char       **end)
+{
+  char *match, *url = NULL;
+  const char *start;
+
+  start = buffer;
+
+  /*
+   * linkify http://,  https://, file://, and www.
+   */
+  while ((match = strpbrk (start, ":."))) {
+    get_url_start (start, match, URL_HTTP, "://", &url);
+    get_url_start (start, match, URL_HTTPS, "://", &url);
+    get_url_start (start, match, URL_FILE, "://", &url);
+    get_url_start (start, match, URL_WWW, ".", &url);
+
+    start = match + 1;
+
+    if (url && url > buffer &&
+        !isspace (url[-1]) && !ispunct (url[-1]))
+      url = NULL;
+
+    if (url)
+      break;
+  }
+
+  if (url) {
+    size_t url_end;
+
+    url_end = strcspn (url, " \n()[],\t\r");
+    if (url_end == strlen (url))
+      *end = url + url_end;
+    else {
+      char *ptr = url + url_end + 1;
+
+      while (ptr[-1] == '(') {
+        size_t count = 0;
+        size_t other_count = 0;
+
+        count = strcspn (ptr, ")");
+        other_count = strcspn (ptr, " \n([],\t\r");
+        if (count < strlen (ptr) && count < other_count)
+          ptr += count + 2;
+        else
+          break;
+      }
+
+      while (!isspace(ptr[-1]) && !isspace(ptr[0]) &&
+             ptr[-1] != '(' && ptr[-1] != ')') {
+        size_t count = 0;
+
+        count = strcspn (ptr, " \n()[],\t\r");
+        ptr += count + 1;
+        if (count == strlen (ptr - count - 1))
+          break;
+      }
+      *end = ptr - 1;
+    }
+  }
+
+  return url;
+}
+
+static char **
+load_tracking_ids (void)
+{
+  GBytes *resource_data;
+  char **lines;
+  g_autoptr(GError) error = NULL;
+  resource_data = g_resources_lookup_data ("/sm/puri/Chatty/tracking_ids/tracking_ids.txt",
+                                          G_RESOURCE_LOOKUP_FLAGS_NONE,
+                                          &error);
+  if (error) {
+    g_warning ("Failed to load resource %s", error->message);
+    return NULL;
+  }
+
+  lines = g_strsplit_set (g_bytes_get_data (resource_data, NULL), "\r\n", 0);
+
+  g_bytes_unref(resource_data);
+
+  return lines;
+}
+
+/*
+ * https://datatracker.ietf.org/doc/html/rfc1738#section-3.3
+ * An URL takes the form: http://<host>:<port>/<path>?<searchpart>
+ *
+ */
+char *
+chatty_utils_strip_utm_from_url (const char *url_to_parse)
+{
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GUri) url = NULL;
+  GString *str = NULL;
+  char *stripped_url, *unowned_attr, *unowned_value;
+  GUriParamsIter iter;
+  char **tracking_ids = NULL;
+
+  if (!url_to_parse || !strstr (url_to_parse, "?"))
+    return g_strdup (url_to_parse);
+
+  url = g_uri_parse (url_to_parse, G_URI_FLAGS_NONE, NULL);
+  if (!url)
+    return g_strdup (url_to_parse);
+
+  tracking_ids = load_tracking_ids ();
+  if (!tracking_ids)
+    return g_strdup (url_to_parse);
+
+  str = g_string_new (NULL);
+  g_uri_params_iter_init (&iter, g_uri_get_query (url), -1, "&", G_URI_PARAMS_NONE);
+  while (g_uri_params_iter_next (&iter, &unowned_attr, &unowned_value, &error)) {
+    g_autofree char *attr = g_steal_pointer (&unowned_attr);
+    g_autofree char *value = g_steal_pointer (&unowned_value);
+    gboolean tracking_id = FALSE;
+
+    for (unsigned int j = 0; tracking_ids[j] != NULL; j++) {
+       /* The g_resource has "//" as comments, ignore those */
+       if (g_str_has_prefix (tracking_ids[j], "//"))
+          continue;
+
+       if (g_strcmp0 ((char *) attr, tracking_ids[j]) == 0) {
+         tracking_id = TRUE;
+         break;
+       }
+    }
+
+    if (!tracking_id) {
+      str = g_string_append (str, attr);
+      str = g_string_append (str, "=");
+      str = g_string_append (str, value);
+      str = g_string_append (str, "&");
+    }
+
+  }
+  if (error)
+    str = g_string_append (str, g_uri_get_query (url));
+
+  /* Remove trailing "&" */
+  if (str->len > 1 && str->str[str->len - 1] == '&')
+    g_string_truncate (str, str->len - 1);
+
+  stripped_url = g_uri_join (G_URI_FLAGS_NONE,
+                             g_uri_get_scheme (url),
+                             g_uri_get_userinfo (url),
+                             g_uri_get_host (url),
+                             g_uri_get_port (url),
+                             g_uri_get_path (url),
+                             (str->len ? str->str : NULL),
+                             g_uri_get_fragment (url));
+
+  g_string_free (str, TRUE);
+  g_strfreev (tracking_ids);
+
+  return stripped_url;
+}
+
+char *
+chatty_utils_strip_utm_from_message (const char *message)
+{
+  GString *str = NULL;
+  char *start, *end, *url;
+
+  if (!message)
+    return NULL;
+
+  if (!*message)
+    return g_strdup ("");
+
+  str = g_string_sized_new (256);
+  start = end = (char *)message;
+
+  while ((url = chatty_utils_find_url (start, &end))) {
+    g_autofree char *link = NULL;
+    g_autofree char *escaped_link = NULL;
+    g_autofree char *utm_stripped_link = NULL;
+
+    link = g_strndup (start, url - start);
+    g_string_append (str, link);
+    g_free (link);
+
+    link = g_strndup (url, end - url);
+    utm_stripped_link = chatty_utils_strip_utm_from_url (link);
+    g_string_append (str, utm_stripped_link);
+    start = end;
+  }
+
+  if (start && *start) {
+    g_string_append (str, start);
+  }
+
+  return g_string_free (str, FALSE);
+}
 
 /*
  * matrix_id_is_valid:
--- tests/message-row.c
+++ tests/message-row.c
@@ -119,47 +119,6 @@ test_message_text_markup (void)
   }
 }
 
-static void
-test_message_strip_utm (void)
-{
-  typedef struct data {
-    char *text;
-    char *check;
-  } data;
-  data array[] = {
-    {
-     "http://www.example.com/user's-image.png?blah=1234",
-     "http://www.example.com/user's-image.png?blah=1234"},
-    {
-     "http://www.example.com/user's-image.png?blah",
-     "http://www.example.com/user's-image.png?blah"},
-    {
-     "http://www.example.com/user's-image.png?utm_source=1234qwer&fbclid=1234564&",
-     "http://www.example.com/user's-image.png"},
-    {
-     "https://www.example.com/?t=ftsa&q=hello&ia=definition",
-     "https://www.example.com/?t=ftsa&q=hello&ia=definition"},
-    {
-     "http://example.com/utm_source/something?v=_utm_source",
-     "http://example.com/utm_source/something?v=_utm_source"},
-    {
-     "http://www.example.com/user's-image.png?_utm_sourcecode=1234qwer&fbclid=1234564&",
-     "http://www.example.com/user's-image.png?_utm_sourcecode=1234qwer"},
-    {
-     "http://utm_source.example.com/something?wowbraid=123",
-     "http://utm_source.example.com/something?wowbraid=123"},
-    {"https://breeo.co/pages/pizza-oven?utm_source=facebook&utm_medium=cpc&utm_campaign=Pizza+Launch+%257C+Full+Funnel+%257C+Conversion%257ERetargeting+Purchase+%257C+Traffic+Engagers+Purchasers&utm_content=Spec+Text+Callouts+IMG+%257C+X24+Pizza+Oven&ad_id=6598924229883&adset_id=6598924227883&campaign_id=6598902108283&ad_name=Spec+Text+Callouts+IMG+%257C+X24+Pizza+Oven&adset_name=Retargeting+Purchase+%257C+Traffic+Engagers+Purchasers&campaign_name=Pizza+Launch+%257C+Full+Funnel+%257C+Conversion&placement=Instagram_Reels",
-     "https://breeo.co/pages/pizza-oven"},
-  };
-
-  for (guint i = 0; i < G_N_ELEMENTS (array); i++) {
-    g_autofree char *content = NULL;
-
-    content = strip_utm (array[i].text);
-    g_assert_cmpstr (content, ==, array[i].check);
-  }
-}
-
 int
 main (int   argc,
       char *argv[])
@@ -167,7 +126,6 @@ main (int   argc,
   g_test_init (&argc, &argv, NULL);
 
   g_test_add_func ("/message-text/markup", test_message_text_markup);
-  g_test_add_func ("/message-text/strip_utm", test_message_strip_utm);
 
   return g_test_run ();
 }
