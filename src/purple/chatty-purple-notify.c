8f7cd378ef8a59806673540433ed0220692fc703
Chris Talbot <chris@talbothome.com>
2024-03-16 19:27:16 -0600

    purple-notify: replace message dialog with alert dialog

--- src/purple/chatty-purple-notify.c
+++ src/purple/chatty-purple-notify.c
@@ -19,25 +19,25 @@ chatty_notify_message (PurpleNotifyMsgType  type,
                        const char          *primary,
                        const char          *secondary)
 {
-  GtkWidget *dialog;
+  AdwDialog *dialog;
   GtkWindow *window;
 
   window = gtk_application_get_active_window (GTK_APPLICATION (g_application_get_default ()));
-  dialog = adw_message_dialog_new (window, _("Error"), NULL);
+  dialog = adw_alert_dialog_new (_("Error"), NULL);
 
-  adw_message_dialog_format_heading (ADW_MESSAGE_DIALOG (dialog),
-                                    "%s",
+  adw_alert_dialog_format_heading (ADW_ALERT_DIALOG (dialog),
+                                    _("%s"),
                                      primary ? primary : title);
 
-  adw_message_dialog_format_body (ADW_MESSAGE_DIALOG (dialog),
-                                  "%s",
+  adw_alert_dialog_format_body (ADW_ALERT_DIALOG (dialog),
+                                  _("%s"),
                                    secondary);
-  adw_message_dialog_add_response (ADW_MESSAGE_DIALOG (dialog), "close", _("Close"));
+  adw_alert_dialog_add_response (ADW_ALERT_DIALOG (dialog), "close", _("Close"));
 
-  adw_message_dialog_set_default_response (ADW_MESSAGE_DIALOG (dialog), "close");
-  adw_message_dialog_set_close_response (ADW_MESSAGE_DIALOG (dialog), "close");
+  adw_alert_dialog_set_default_response (ADW_ALERT_DIALOG (dialog), "close");
+  adw_alert_dialog_set_close_response (ADW_ALERT_DIALOG (dialog), "close");
 
-  gtk_window_present (GTK_WINDOW (dialog));
+  adw_dialog_present (dialog, GTK_WIDGET (window));
 
   return dialog;
 }
