59a7242b75d92253d5758031643c7f155bde7841
Guido Günther <agx@sigxcpu.org>
2024-09-17 21:51:10 +0200

    treewide: Use --gapplication-service
    Instead of open coding most of what this option does just use it and can
    hence drop --daemon and the `show_window` vs `daemon` logic. This will
    allow us to use DBus activation.
    
    Part-of: <https://gitlab.gnome.org/World/Chatty/-/merge_requests/1432>

--- src/chatty-application.c
+++ src/chatty-application.c
@@ -60,9 +60,6 @@ struct _ChattyApplication
 
   char *uri;
   guint open_uri_id;
-
-  gboolean daemon;
-  gboolean show_window;
 };
 
 G_DEFINE_TYPE (ChattyApplication, chatty_application, ADW_TYPE_APPLICATION)
@@ -74,7 +71,6 @@ static gboolean    cmd_verbose_cb   (const char *option_name,
 
 static GOptionEntry cmd_options[] = {
   { "version", 0, G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, NULL, N_("Show release version"), NULL },
-  { "daemon", 'D', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, NULL, N_("Start in daemon mode"), NULL },
   { "nologin", 'n', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, NULL, N_("Disable all accounts"), NULL },
 #ifdef PURPLE_ENABLED
   { "debug", 'd', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, NULL, N_("Enable libpurple debug messages"), NULL },
@@ -231,7 +227,6 @@ chatty_application_show_window (GSimpleAction *action,
 
   g_assert (CHATTY_IS_APPLICATION (self));
 
-  self->show_window = TRUE;
   g_application_activate (G_APPLICATION (self));
 }
 
@@ -264,7 +259,6 @@ chatty_application_open_chat (GSimpleAction *action,
     g_info ("%s", str->str);
   }
 
-  self->show_window = TRUE;
   g_application_activate (G_APPLICATION (self));
   chatty_window_open_chat (CHATTY_WINDOW (self->main_window), chat);
 }
@@ -355,20 +349,6 @@ chatty_application_command_line (GApplication            *application,
     chatty_purple_enable_debug ();
 #endif
 
-  self->show_window = TRUE;
-  if (g_variant_dict_contains (options, "daemon")) {
-    /* Hold application only the first time daemon mode is set */
-    if (!self->daemon)
-      g_application_hold (application);
-
-    chatty_manager_load (self->manager);
-
-    self->show_window = FALSE;
-    self->daemon = TRUE;
-
-    g_debug ("Enable daemon mode");
-  }
-
   arguments = g_application_command_line_get_arguments (command_line, &argc);
 
   /* Keep only the last URI, if there are many */
@@ -398,7 +378,6 @@ chatty_application_startup (GApplication *application)
   g_autofree char *purple_dir = NULL;
   const char *help_accels[] = { "F1", NULL };
 
-  self->daemon = FALSE;
   self->manager = chatty_manager_get_default ();
 
   G_APPLICATION_CLASS (chatty_application_parent_class)->startup (application);
@@ -429,6 +408,13 @@ chatty_application_startup (GApplication *application)
   g_action_map_add_action_entries (G_ACTION_MAP (self), app_entries,
                                    G_N_ELEMENTS (app_entries), self);
   gtk_application_set_accels_for_action (GTK_APPLICATION (self), "app.help", help_accels);
+
+  if (g_application_get_flags (application) & G_APPLICATION_IS_SERVICE) {
+    g_application_hold (application);
+
+    chatty_manager_load (self->manager);
+    g_debug ("Enable daemon mode");
+  }
 }
 
 
@@ -442,7 +428,7 @@ chatty_application_activate (GApplication *application)
 
   chatty_manager_load (self->manager);
 
-  if (!self->main_window && self->show_window) {
+  if (!self->main_window) {
     g_set_weak_pointer (&self->main_window, chatty_window_new (app));
     g_info ("New main window created");
 
@@ -451,8 +437,7 @@ chatty_application_activate (GApplication *application)
                              self, G_CONNECT_SWAPPED);
   }
 
-  if (self->show_window)
-    gtk_window_present (GTK_WINDOW (self->main_window));
+  gtk_window_present (GTK_WINDOW (self->main_window));
 
   /* Open with some delay so that the modem is ready when not in daemon mode */
   if (self->uri)
