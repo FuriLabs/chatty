81cf057e019e95f6eb3faaf2f0cefe3b06a58345
Guido Günther <agx@sigxcpu.org>
2024-10-09 10:41:27 +0200

    clock: Use PRIi64 instead of G_GINT64_FORMAT
    This is supported by gettext. See
    https://www.gnu.org/software/gettext/manual/html_node/Preparing-Strings.html
    and https://savannah.gnu.org/bugs/index.php?66313
    
    This then turns up as
    
       msgid "%<PRIi64> minute ago"
       msgid_plural "%<PRIi64> minutes ago"
    
    in the message catalog.
    
    Closes: https://gitlab.gnome.org/World/Chatty/-/issues/948
    Part-of: <https://gitlab.gnome.org/World/Chatty/-/merge_requests/1443>

--- src/chatty-clock.c
+++ src/chatty-clock.c
@@ -14,6 +14,8 @@
 # include "config.h"
 #endif
 
+#include <inttypes.h>
+
 #include <glib/gi18n.h>
 #include <gdesktop-enums.h>
 
@@ -100,8 +102,8 @@ clock_get_human_time (ChattyClock         *self,
 
   if (detailed && time_span < G_TIME_SPAN_HOUR)
     return g_strdup_printf (g_dngettext (GETTEXT_PACKAGE,
-                                         "%" G_GINT64_FORMAT " minute ago",
-                                         "%" G_GINT64_FORMAT " minutes ago",
+                                         "%" PRIi64 " minute ago",
+                                         "%" PRIi64 " minutes ago",
                                          time_span / G_TIME_SPAN_MINUTE),
                             time_span / G_TIME_SPAN_MINUTE);
   /* The day can be same, but from two different years, so check the time span too */
